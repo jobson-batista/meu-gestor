2da4d8e3b343270f324dcc78c8a282507c0216ac
Jobson Batista <51756630+jobson-batista@users.noreply.github.com>
2026-04-20 13:48:21 -0300

    Create DebtRepository

---
+++ backend/src/main/java/com/meugestor/repositories/DebtRepository.java
@@ -0,0 +1,8 @@
+package com.meugestor.repositories;
+
+import com.meugestor.entities.Debt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface DebtRepository extends JpaRepository<Debt, UUID> {
+}
