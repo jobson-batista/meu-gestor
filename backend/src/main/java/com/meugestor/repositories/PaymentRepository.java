691278579b15ad1974e16913dda77b402c35b127
Jobson Batista <51756630+jobson-batista@users.noreply.github.com>
2026-04-20 13:48:27 -0300

    Create PaymentRepository

---
+++ backend/src/main/java/com/meugestor/repositories/PaymentRepository.java
@@ -0,0 +1,8 @@
+package com.meugestor.repositories;
+
+import com.meugestor.entities.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+}
