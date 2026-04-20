7ef6923d4065f509df7ae9396ff70e3302d05f8f
Jobson Batista <51756630+jobson-batista@users.noreply.github.com>
2026-04-20 13:44:38 -0300

    Create main Spring Boot application class

---
+++ backend/src/main/java/com/meugestor/MeuGestorApplication.java
@@ -0,0 +1,13 @@
+package com.meugestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuGestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuGestorApplication.class, args);
+    }
+
+}
